4e7dd4ad7edeabc9ea4e4a38508f215b343605ae
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-04-02 09:57:05 +0800

    Update register.js

---
+++ src/pages/register.js
@@ -0,0 +1,9 @@
+import RegisterForm from '../components/forms/registerForm'
+
+export default function RegisterPage() {
+    return (
+        <div>
+            <RegisterForm />
+        </div>
+    )
+}
