36363506be4cc713df60647708b056cb1d7bcb4b
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-04-07 18:55:42 +0800

    Completed css for Registration Form

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -2,7 +2,8 @@ import RegisterForm from '../components/forms/registerForm'
 
 export default function RegisterPage() {
     return (
-        <div>
+        <div className="bg-gray-100" >
+            <h1 className="text-center uppercase tracking-wide text-black text-xl font-bold mb-2 py-10">Registration Form</h1>
             <RegisterForm />
         </div>
     )
