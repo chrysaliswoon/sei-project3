f43475c794c5ac3eb4bd7af4517d7ba8a1795a9d
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-04-16 14:04:38 +0800

    Update finances

--- src/pages/finances.jsx
+++ src/pages/finances.jsx
@@ -4,6 +4,7 @@ import TransactionForm from "../components/forms/transactionForm";
 export default function () {
   const URL = BACKEND + "/transactions";
   const [account, setAccount] = []
+  console.log(account)
 
   const accountData = () => {
     console.log(accountData);
@@ -15,18 +16,18 @@ export default function () {
       }))
   }
 
-  const accountList = ["test1", "test2"];
+  // const accountList = ["test1", "test2"];
 
-  const accountName = accountList.map((account) => (
-    <option value={account}>{account}</option>
-  ));
+  // const accountName = account.map((account) => (
+  //   <option value={account}>{account}</option>
+  // ));
 
   return (
     <div className="bg-gray-100">
       <h1 className="text-center uppercase tracking-wide text-black text-xl font-bold mb-2 py-5">
         New Transaction
       </h1>
-      <TransactionForm account={accountName} />
+      <TransactionForm  />
     </div>
   );
 }
