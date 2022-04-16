548a50339173f5112c8304993b63d4e41523c34c
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-04-16 14:09:23 +0800

    Updated finances form and fixed the usestate

--- src/pages/finances.jsx
+++ src/pages/finances.jsx
@@ -1,33 +1,36 @@
+import React, { useState } from "react";
 import { BACKEND } from "../utils";
 import TransactionForm from "../components/forms/transactionForm";
 
 export default function () {
   const URL = BACKEND + "/transactions";
-  const [account, setAccount] = []
-  console.log(account)
+  const [account, setAccount] = useState([]);
+  console.log(account);
 
   const accountData = () => {
     console.log(accountData);
-    
+
     fetch(URL)
-      .then(response => response.json())
-      .then(data => setAccount({
-        account: data
-      }))
-  }
+      .then((response) => response.json())
+      .then((data) =>
+        setAccount({
+          account: data,
+        })
+      );
+  };
 
   // const accountList = ["test1", "test2"];
 
-  // const accountName = account.map((account) => (
-  //   <option value={account}>{account}</option>
-  // ));
+  const accountName = account.map((account) => (
+    <option value={account}>{account}</option>
+  ));
 
   return (
     <div className="bg-gray-100">
       <h1 className="text-center uppercase tracking-wide text-black text-xl font-bold mb-2 py-5">
         New Transaction
       </h1>
-      <TransactionForm  />
+      <TransactionForm account={accountName}/>
     </div>
   );
 }
