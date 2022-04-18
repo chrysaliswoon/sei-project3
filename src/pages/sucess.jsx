d0f46d9356ab1c8c168c6727776b40ef4c80943e
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-04-19 02:50:51 +0800

    uninstalled sweet alert

--- src/pages/sucess.jsx
+++ src/pages/sucess.jsx
@@ -1,16 +1,20 @@
-import SuccessImage from "../assets/money.gif";
-import { Link } from "react-router-dom";
+import SuccessImage from '../assets/money.gif'
+import { Link } from 'react-router-dom';
 
 export default function RegistrationSuccess() {
-  return (
-    <div className="text-4xl font-normal leading-normal mt-0 mb-2 text-blue-800 text-center">
-      <h1>We've received your registration!</h1>
-      <img
-        className="justify-center"
-        src={SuccessImage}
-        alt="success registration"
-      />
-      <Link to="/">Click here to login to your account</Link>
-    </div>
-  );
+    return (
+      <div className="text-4xl font-normal leading-normal mt-0 mb-2 text-blue-800 text-center">
+        <h1>
+          We've received your registration!
+        </h1>
+            <img
+                className="justify-center"
+                src={SuccessImage} alt="success registration" />
+          <Link
+            to="/"
+          >
+            Click here to login to your account
+          </Link>
+      </div>
+    );
 }
