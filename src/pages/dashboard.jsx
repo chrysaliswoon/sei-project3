9e600739f17aaa5ecf099c0e9bdfbb129060c2f2
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-04-19 01:18:53 +0800

    Install Chart JS

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,5 +1,6 @@
 import SideNavigation from '../components/navigation/navbar'
 import Accounts from '../components/features/dashboard/accounts'
+import TransactionTable from '../components/features/transactions/transactionTable';
 
 export default function Dashboard() {
   return (
@@ -7,11 +8,19 @@ export default function Dashboard() {
       <div className="flex flex-col w-64 h-screen px-4 py-8 overflow-y-auto border-r">
         <SideNavigation />
       </div>
-      <div className="w-md h-full p-4 m-8 overflow-y-auto">
+      <div className=" p-4 m-8">
         <h1 className="uppercase font-bold">Financial Dashboard</h1>
         <p>Get an overview of your financial transactions here</p>
-        <div className="bg-slate-500 px-10">
-          <Accounts />
+        <div className="grid grid-cols-2 gap-4">
+          <div className="bg-slate-500 px-10">
+            <Accounts />
+          </div>
+          <div className="bg-slate-500 px-10">
+            <p>Goals Carousel</p>
+          </div>
+          <div className="bg-slate-500 px-10">
+            <TransactionTable />
+          </div>
         </div>
       </div>
     </div>
